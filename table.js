1ab7b9838deb027f1843c5edffc56795dde09ca2
ytliu0 <yuktungliu@gmail.com>
2019-07-29 21:57:03 -0500

    minor updates

--- table.js
+++ table.js
@@ -1709,11 +1709,11 @@ function printWarningMessage(y, langCon) {
     
     if (y==240 && langCon.region=='Wu') {
         if (lang==0) {
-           warn ='<p style="color:red;">In Appendix 2 of the book <i>3500 Years of Calendars and Astronomical Phenomena</i>, the sexagenary day of the leap month conjunction in 238 is listed as j&#464; ch&#466;u, corresponding to Nov. 25. This is at odds with my calculation. The result of my calculation is consistent with the data on the <a href="https://sinocal.sinica.edu.tw/" target="_blank">Chinese-Western calendar conversion website</a> created by Academia Sinica in Taiwan (sadly, this website was closed on July 1, 2019). The preface of the book says that the calendar data in its appendices are based on the book 《歷代長術輯要》(<i>Compilation of Historical Calendars</i>) by W&#257;ng Yu&#275;zh&#275;n (汪曰楨). I looked at the book and found that the date listed there was also the same as my calculation. I suspect that the date listed in <i>3500 Years of Calendars and Astronomical Phenomena</i> is wrong. The book also lists the sexagenary day of the month 11 conjunction in the Wu state as j&#464; ch&#466;u, which is certainly wrong since this date was far away from the new moon close to the beginning of month 11.</p>'; 
+           warn ='<p style="color:red;">In Appendix 2 of the book <i>3500 Years of Calendars and Astronomical Phenomena</i>, the sexagenary day of the leap month conjunction in 238 is listed as j&#464; ch&#466;u, corresponding to Nov. 25. This is at odds with my calculation. The result of my calculation is consistent with the data on the <a href="http://sinocal.sinica.edu.tw/" target="_blank">Chinese-Western calendar conversion website</a> created by Academia Sinica in Taiwan. The preface of the book says that the calendar data in its appendices are based on the book 《歷代長術輯要》(<i>Compilation of Historical Calendars</i>) by W&#257;ng Yu&#275;zh&#275;n (汪曰楨). I looked at the book and found that the date listed there was also the same as my calculation. I suspect that the date listed in <i>3500 Years of Calendars and Astronomical Phenomena</i> is wrong. The book also lists the sexagenary day of the month 11 conjunction in the Wu state as j&#464; ch&#466;u, which is certainly wrong since this date was far away from the new moon close to the beginning of month 11.</p>'; 
         } else if (lang==1) {
-            warn ='<p style="color:red;">《三千五百年历日天象》附表2記吳赤烏元年閏十月己丑朔和十一月己丑朔。十一月己丑朔無疑是錯的，這裡列出的閏十月戊子朔是根據我的推步，結果與台灣中央研究院的<a href="https://sinocal.sinica.edu.tw/" target="_blank">兩千年中西曆轉換網站</a>一致(很可惜，此網站已於2019年7月1日關閉)，《三千五百年历日天象》前言說其附表參照清汪曰楨的《歷代長術輯要》，翻查此書發現亦記吳閏十月戊子。</p>';
+            warn ='<p style="color:red;">《三千五百年历日天象》附表2記吳赤烏元年閏十月己丑朔和十一月己丑朔。十一月己丑朔無疑是錯的，這裡列出的閏十月戊子朔是根據我的推步，結果與台灣中央研究院的<a href="http://sinocal.sinica.edu.tw/" target="_blank">兩千年中西曆轉換網站</a>一致，《三千五百年历日天象》前言說其附表參照清汪曰楨的《歷代長術輯要》，翻查此書發現亦記吳閏十月戊子。</p>';
         } else {
-            warn = '<p style="color:red;">《三千五百年历日天象》附表2记吴赤乌元年闰十月己丑朔和十一月己丑朔。十一月己丑朔无疑是错的，这里列出的闰十月戊子朔是根据我的推步，结果与台湾中央研究院的<a href="https://sinocal.sinica.edu.tw/" target="_blank">两千年中西历转换网站</a>一致(很可惜，此网站已于2019年7月1日关闭)，《三千五百年历日天象》前言说其附表参照清汪曰桢的《历代长术辑要》，翻查此书发现亦记吴闰十月戊子。</p>';
+            warn = '<p style="color:red;">《三千五百年历日天象》附表2记吴赤乌元年闰十月己丑朔和十一月己丑朔。十一月己丑朔无疑是错的，这里列出的闰十月戊子朔是根据我的推步，结果与台湾中央研究院的<a href="http://sinocal.sinica.edu.tw/" target="_blank">两千年中西历转换网站</a>一致，《三千五百年历日天象》前言说其附表参照清汪曰桢的《历代长术辑要》，翻查此书发现亦记吴闰十月戊子。</p>';
         }
     }
     
@@ -1739,11 +1739,11 @@ function printWarningMessage(y, langCon) {
     
     if (y==577 && langCon.region=='WeiQi') {
         if (lang==0) {
-            warn ="<p style='color:red;'>Appendix 3 of the book <i>3500 Years of Calendars and Astronomical Phenomena</i> lists the leap month in 575 as after month 9. This is at odds with my calculation, which agrees with the data on the <a href='https://sinocal.sinica.edu.tw/' target='_blank'>Chinese-Western calendar conversion website</a> created by Academia Sinica in Taiwan (sadly, this website was closed on July 1, 2019). The data in Appendix 3 are supposed to be based on the book 《歷代長術輯要》(<i>Compilation of Historical Calendars</i>) by W&#257;ng Yu&#275;zh&#275;n (汪曰楨), but that book also lists 575's leap month as after month 8. That's why I use my calculation here.</p>";
+            warn ="<p style='color:red;'>Appendix 3 of the book <i>3500 Years of Calendars and Astronomical Phenomena</i> lists the leap month in 575 as after month 9. This is at odds with my calculation, which agrees with the data on the <a href='http://sinocal.sinica.edu.tw/' target='_blank'>Chinese-Western calendar conversion website</a> created by Academia Sinica in Taiwan. The data in Appendix 3 are supposed to be based on the book 《歷代長術輯要》(<i>Compilation of Historical Calendars</i>) by W&#257;ng Yu&#275;zh&#275;n (汪曰楨), but that book also lists 575's leap month as after month 8. That's why I use my calculation here.</p>";
         } else if (lang==1) {
-            warn ='<p style="color:red;">《三千五百年历日天象》附表3記北齊武平六年為閏九月，與我計算的閏八月不一致，台灣中央研究院的<a href="https://sinocal.sinica.edu.tw/" target="_blank">兩千年中西曆轉換網站</a>(很可惜，此網站已於2019年7月1日關閉)和汪曰楨的《歷代長術輯要》也記北齊武平六年為閏八月，所以這裡不取《三千五百年历日天象》的數據。</p>';
+            warn ='<p style="color:red;">《三千五百年历日天象》附表3記北齊武平六年為閏九月，與我計算的閏八月不一致，台灣中央研究院的<a href="http://sinocal.sinica.edu.tw/" target="_blank">兩千年中西曆轉換網站</a>和汪曰楨的《歷代長術輯要》也記北齊武平六年為閏八月，所以這裡不取《三千五百年历日天象》的數據。</p>';
         } else {
-            warn = '<p style="color:red;">《三千五百年历日天象》附表3记北齐武平六年为闰九月，与我计算的闰八月不一致，台湾中央研究院的<a href="https://sinocal.sinica.edu.tw/" target="_blank">两千年中西历转换网站</a>(很可惜，此网站已于2019年7月1日关闭)和汪曰桢的《历代长术辑要》也记北齐武平六年为闰八月，所以这里不取《三千五百年历日天象》的数据。</p>';
+            warn = '<p style="color:red;">《三千五百年历日天象》附表3记北齐武平六年为闰九月，与我计算的闰八月不一致，台湾中央研究院的<a href="http://sinocal.sinica.edu.tw/" target="_blank">两千年中西历转换网站</a>和汪曰桢的《历代长术辑要》也记北齐武平六年为闰八月，所以这里不取《三千五百年历日天象》的数据。</p>';
         }
     }
     
